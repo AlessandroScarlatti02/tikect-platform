abb02c75aba78d6a3ec4ea95b19bae4d773efc23
AlessandroScarlatti02 <alessandro.scarlatti2002@gmail.com>
2024-12-09 10:35:03 +0100

    First Commit

---
+++ src/main/java/it/milestone/exam/tikect_platform/TikectPlatformApplication.java
@@ -0,0 +1,13 @@
+package it.milestone.exam.tikect_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikectPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikectPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/milestone/exam/tikect_platform/TikectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.milestone.exam.tikect_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikectPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
